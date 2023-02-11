6515ebd6db9571eb605ff604693023f53f20c6ad
devyan18 <epache17@gmail.com>
2023-02-11 15:52:12 -0300

    Added cors config to cors middleware

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express()
 app.use(express.json())
 app.use(morgan('combined'))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}))
 
 app.post('/track', async (req, res) => {
   const trackId = req.body.track
